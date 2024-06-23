ea52ee192fc906040b907a3fe452a75b9a69df9a
Andile18 <zama.andile18@gmail.com>
2024-06-23 17:51:48 +0200

    Update FormartDate.js and addect correct date and time

--- src/FormatDate.js
+++ src/FormatDate.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 
+
 export default function FormatDate (props){
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day = days[props.date.getDay()];
