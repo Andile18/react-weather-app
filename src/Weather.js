b58b51562bf749317c05a2ffdb3548952f60d87f
Andile18 <zama.andile18@gmail.com>
2024-06-30 03:34:06 +0200

    update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -28,7 +28,7 @@ export default function Weather (props){
 
 
         const apiKey= "d0fb080483e20b38f546f3709468468e";
-        let apiUrl= `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
         axios.get(apiUrl).then(handleResponse);
           
 
