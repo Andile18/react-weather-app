ca44f76bcbf7fc039fd88f6836e7dc2fb879b2e0
Andile18 <zama.andile18@gmail.com>
2024-07-01 22:29:50 +0200

    Update WeatherIcon.js

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -33,7 +33,7 @@ export default function WeatherIcon(props){
         <ReactAnimatedWeather
         icon={codeMapping[props.code]}
         color="blue"
-        size={50}
+        size={props.size}
         animate={true}
       />
     );
