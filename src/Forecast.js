330c4c5235c1bbcc05db525c797d8ca46ff6ec77
Andile18 <zama.andile18@gmail.com>
2024-07-03 23:07:41 +0200

    Update Forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,9 +1,27 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./Forecast.css";
+import axios from "axios";
 
 
-export default function Forecast(){
+export default function Forecast(props){
+
+
+    function handleResponse(response){
+      
+      console.log(response.data);
+    }
+   
+
+    let apiKey= "3f6be1c407b0d9d1933561808db358ba";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    
+    axios.get(apiUrl).then(handleResponse);
+
+
     return (
         <div className="Forecast"> 
         <div className="row">
